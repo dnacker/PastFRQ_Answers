7a818203b689f9fd1f36036ee293514904ee59fa
daackerman <daackerman@seattleschools.org>
2017-02-09 14:56:42 -0800

    initial commit

---
+++ FRQ_PRACTICE_EXAM_A/src/Q1/WordScrambler.java
@@ -0,0 +1,55 @@
+package Q1;
+
+import java.util.Arrays;
+
+/**
+ * Created by David on 1/22/2017.
+ */
+public class WordScrambler {
+    private String[] scrambleWords;
+
+    public WordScrambler(String[] wordArr) {
+        scrambleWords = mixedWords(wordArr);
+    }
+
+    private String recombine(String word1, String word2) {
+        return word1.substring(0, word1.length()/2) + word2.substring(word2.length()/2);
+    }
+
+
+    /*
+    private String[] mixedWords(String[] words) {
+        String[] mixed = new String[words.length];
+        int index = 0;
+        for (int i = 0; i < words.length/2; i++) {
+            mixed[index] = recombine(words[2*i], words[2*i + 1]);
+            index++;
+            mixed[index] = recombine(words[2*i + 1], words[2*i]);
+            index++;
+        }
+        return mixed;
+    }
+    */
+    private String[] mixedWords(String[] words) {
+        String[] newWords = new String[words.length];
+        for (int i = 0; i < words.length; i++) {
+            if (i % 2 == 0) {
+                newWords[i] = recombine(words[i], words[i + 1]);
+            } else {
+                newWords[i] = recombine(words[i], words[i - 1]);
+            }
+        }
+        return newWords;
+    }
+
+
+    public String toString() {
+        return Arrays.toString(scrambleWords);
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"apple", "pear", "this", "cat"};
+        WordScrambler scrambler = new WordScrambler(words);
+        System.out.println(scrambler);
+    }
+}
---
+++ FRQ_PRACTICE_EXAM_A/src/Q3/TemperatureGrid.java
@@ -0,0 +1,59 @@
+package Q3;
+
+/**
+ * Created by David on 1/22/2017.
+ */
+public class TemperatureGrid {
+    private double[][] temps;
+
+    public TemperatureGrid(double[][] grid) {
+        temps = grid;
+    }
+
+    private double computeTemp(int row, int col) {
+        if (row > 0 && col > 0 && row < temps.length - 1 && col < temps[0].length - 1) {
+            return (temps[row - 1][col] + temps[row + 1][col] + temps[row][col - 1] + temps[row][col + 1]) / 4;
+        } else {
+            return temps[row][col];
+        }
+    }
+
+    public boolean updateAllTemps(double tolerance) {
+        double[][] newGrid = new double[temps.length][temps[0].length];
+        boolean res = true;
+        for (int row = 0; row < temps.length; row++) {
+            for (int col = 0; col < temps[0].length; col++) {
+                double newTemp = computeTemp(row, col);
+                if (Math.abs(newTemp - temps[row][col]) > tolerance) {
+                    res = false;
+                }
+                newGrid[row][col] = newTemp;
+            }
+        }
+        temps = newGrid;
+        return res;
+    }
+
+    public String toString() {
+        String res = "";
+        for (int row = 0; row < temps.length; row++) {
+            for (int col = 0; col < temps[0].length; col++) {
+                res += temps[row][col] + " ";
+            }
+            res += "\n";
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TemperatureGrid temps = new TemperatureGrid(new double[][]{{95.5, 100.0, 100.0, 100.0, 100.0, 110.3},
+                                                                    {0.0, 50.0, 50, 50, 50, 0},
+                                                                    {0, 40, 40, 40, 40, 0},
+                                                                    {0, 20, 20, 20, 20, 0},
+                                                                    {0, 0, 0, 0, 0, 0}});
+        System.out.println(temps);
+        boolean within = temps.updateAllTemps(0.01);
+        System.out.println(temps);
+        System.out.println(within);
+    }
+}
